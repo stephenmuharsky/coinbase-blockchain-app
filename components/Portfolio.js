7b42c97193a4be4b70b6b32efd5220ca393a8e06
Stephen Muharsky <smuharsk@gmail.com>
2022-02-23 11:05:06 -0500

    Added list of coins

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,5 +1,8 @@
 import React from "react"
 import styled from "styled-components"
+import { BsThreeDotsVertical } from "react-icons/bs"
+import { coins } from "../static/coins"
+import Coin from "./Coin"
 
 const Portfolio = () => {
   return (
@@ -8,6 +11,29 @@ const Portfolio = () => {
         <TableItem>
           <Title>Your Assets</Title>
         </TableItem>
+        <Divider />
+        <Table>
+          <TableItem>
+            <TableRow>
+              <div style={{ flex: 3 }}>Name</div>
+              <div style={{ flex: 2 }}>Balance</div>
+              <div style={{ flex: 1 }}>Price</div>
+              <div style={{ flex: 1 }}>Allocation</div>
+              <div style={{ flex: 0 }}>
+                <BsThreeDotsVertical />
+              </div>
+            </TableRow>
+          </TableItem>
+          <Divider />
+          <div>
+            {coins.map((coin) => (
+              <div>
+                <Coin coin={coin} />
+                <Divider />
+              </div>
+            ))}
+          </div>
+        </Table>
       </PortfolioTable>
     </Wrapper>
   )
